2f8a057211a61216cb050911f2394e5f6d33d2fd
Sinod1996 <cool.sinod@mail.ru>
2023-01-07 20:54:33 +0400

    complete the header functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
 import {useLocation, useNavigate} from "react-router";
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {getAuth, onAuthStateChanged} from "firebase/auth";
 
 function Header(props) {
+    const [pageState, setPageState] = useState('Sign in')
     const location = useLocation();
     const navigate = useNavigate();
-    function pathMathRoute(route) {
+    const auth = getAuth();
+    useEffect(() => {
+         onAuthStateChanged(auth, (user) => {
+             if (user) {
+                 setPageState('Profile')
+             } else {
+                 setPageState('Sign in')
+             }
+         })
+    }, [auth])
+    function pathMatchRoute(route) {
         if (route === location.pathname) {
             return true
         }
@@ -24,22 +36,22 @@ function Header(props) {
                     <ul className={'flex space-x-10'}>
                         <li
                             className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] 
-                        border-b-transparent ${pathMathRoute("/") && "text-black border-b-red-500"}`}
+                        border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"}`}
                             onClick={() => navigate("/")}
                         >
                             Home
                         </li>
                         <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] 
-                        border-b-transparent ${pathMathRoute("/offers") && "text-black border-b-red-500"}`}
+                        border-b-transparent ${pathMatchRoute("/offers") && "text-black border-b-red-500"}`}
                             onClick={() => navigate("/offers")}
                         >
                             Offers
                         </li>
                         <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] 
-                        border-b-transparent ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`}
-                            onClick={() => navigate("/sign-in")}
+                        border-b-transparent ${(pathMatchRoute("/sign-in") || pathMatchRoute("/profile") ) && "text-black border-b-red-500"}`}
+                            onClick={() => navigate("/profile")}
                         >
-                            Sign in
+                            {pageState}
                         </li>
                     </ul>
                 </div>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -36,8 +36,8 @@ function SignUp(props) {
             delete formDataCopy.password;
             formDataCopy.timestamp = serverTimestamp();
             await setDoc(doc(db, "users", user.uid), formDataCopy);
-            // toast.success("Sign up was successful");
-            // navigate("/");
+            toast.success("Sign up was successful");
+            navigate("/");
         } catch (error) {
             toast.error('Something went wrong with the registration')
         }
